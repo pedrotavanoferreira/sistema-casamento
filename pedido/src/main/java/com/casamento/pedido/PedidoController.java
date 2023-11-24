931f37fee718249ba2f2cc5dd56f36e51abd1156
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2023-11-24 09:54:05 -0300

    adiciona post

--- pedido/src/main/java/com/casamento/pedido/PedidoController.java
+++ pedido/src/main/java/com/casamento/pedido/PedidoController.java
@@ -4,7 +4,7 @@ import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +15,7 @@ public class PedidoController {
 
     private static final Logger LOG = LoggerFactory.getLogger(PedidoController.class);
 
-    @GetMapping
+    @PostMapping
     @Operation(summary = "Endpoint para informar o seu nome mágico")
     public ResponseEntity<String> buscarPedido() {
         return ResponseEntity.ok("ok");
