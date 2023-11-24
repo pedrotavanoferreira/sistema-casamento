c0f54aa8823e8a5d5a42a3b2de5c7c550ec4739d
Pedro Tavano Ferreira <pedro_ferry@outlook.com>
2023-11-24 09:50:20 -0300

    adiciona endpoint

---
+++ pedido/src/main/java/com/casamento/pedido/PedidoController.java
@@ -0,0 +1,24 @@
+package com.casamento.pedido;
+
+import io.swagger.v3.oas.annotations.Operation;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/pedido")
+public class PedidoController {
+
+
+    private static final Logger LOG = LoggerFactory.getLogger(PedidoController.class);
+
+    @GetMapping
+    @Operation(summary = "Endpoint para informar o seu nome mágico")
+    public ResponseEntity<String> buscarPedido() {
+        return ResponseEntity.ok("ok");
+    }
+
+}
